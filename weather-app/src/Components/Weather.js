63d75737576c9a99a33cd58c5e2cdb5169a3f5ba
Noor Muhammad <noormohd45.nm@gmail.com>
2019-05-04 00:40:32 +0500

    weather icon added

--- weather-app/src/Components/Weather.js
+++ weather-app/src/Components/Weather.js
@@ -1,6 +1,25 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCloud, faSun, faCloudRain, faBolt } from '@fortawesome/free-solid-svg-icons'
+
+const weatherIcon = [
+    { condition: 'few clouds', icon: faCloud },
+    { condition: 'clear sky', icon: faSun },
+    { condition: 'shower rain', icon: faCloudRain },
+    { condition: 'thunderstorm', icon: faBolt}
+]
+
 class Weather extends React.Component {
+
+    getIcon = (condition) => {
+        const temp = weatherIcon.find(weather => {
+            return weather.condition === condition
+        })
+        if(temp)
+            return temp.icon
+    }
+
     render() {
         return (
             <div className="weather__info">
@@ -13,19 +32,20 @@ class Weather extends React.Component {
                     <p className="weather__key"> Temperature: <span className="weather__value"> {this.props.temperature}</span></p>
                 }
                 {
-                    this.props.humidity && 
+                    this.props.humidity &&
                     <p className="weather__key">Humidity: <span className="weather__value"> {this.props.humidity}</span></p>
                 }
                 {
-                    this.props.windSpeed && 
+                    this.props.windSpeed &&
                     <p className="weather__key">Wind Speed: <span className="weather__value"> {this.props.windSpeed}</span></p>
                 }
                 {
-                    this.props.desc && 
-                    <p className="weather__key">Conditions: <span className="weather__value"> {this.props.desc}</span></p>
+                    this.props.desc &&
+                    <p className="weather__key">Conditions: <span className="weather__value"> {this.props.desc}</span>
+                        <FontAwesomeIcon className="weather__icon" icon = {this.getIcon(this.props.desc)} />                    </p>
                 }
                 {
-                    this.props.err && 
+                    this.props.err &&
                     <p className="weather__error">Invalid Values: {this.props.err}</p>
                 }
             </div>
